9529a421427c8e0ec1777a31bc0d749397fdcc9c
Alex Westerman <westeraj@rose-hulman.edu>
2021-02-15 11:33:14 -0500

    Cleaning up a mess
    -Added References Used Related to HID Profile implementation
    -Added Open Source Licenses for Our Code and TI code
    -Did some manual reformatting in some files to conform with our style
    -Updated some Bluetooth device strings

--- firmware/main_tirtos.c
+++ firmware/main_tirtos.c
@@ -1,51 +1,30 @@
-/* --COPYRIGHT--,BSD
- * Copyright (c) 2017, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * --/COPYRIGHT--*/
-/* External Defines */
-#include <stdint.h>
-#include <ti/sysbios/BIOS.h>
-#include <ti/display/Display.h>
-#include <ti/drivers/GPIO.h>
-#include <ti/drivers/Power.h>
-#include <ti/drivers/Timer.h>
-#include <ti/drivers/UART.h>
+/***********************************************************************
+ * \file 		main_tirtos.c
+ * \author		Texas Instruments
+ * \author 		Alex Westerman
+ * \brief 		Entry point code for TI RTOS
+ * \copyright 	See LICENSE_TI and LICENSE
+ ***********************************************************************/
 
-/* Standard Defines */
-#include "Board.h"
-#include "simple_application_processor.h"
+/***********************************************************************
+ * 								INCLUDE
+***********************************************************************/
+#include <stdint.h>							//Standard C Integer Library
+#include <ti/sysbios/BIOS.h>				//TI RTOS SYS/BIOS Headers
+#include <ti/display/Display.h>				//TI RTOS Display Library
+#include <ti/drivers/GPIO.h>				//TI RTOS GPIO Library
+#include <ti/drivers/Power.h>				//TI RTOS Power Management Library
+#include <ti/drivers/Timer.h>				//TI RTOS Timer Library
+#include <ti/drivers/UART.h>				//TI RTOS UART Library
 
-/* Output display handle that will be used to print out all debug/log
- * statements
- */
-Display_Handle displayOut;
+#include "Board.h"							//MSP432-specific Code
+#include "simple_application_processor.h"	//Custom Bluetooth Library
+
+
+/***********************************************************************
+ * 						VARIABLE DECLARATIONS
+***********************************************************************/
+Display_Handle displayOut;	//Output display handle that will be used to print out all debug/log statements
 
 int main(void) {
 	/* Call board initialization functions */
