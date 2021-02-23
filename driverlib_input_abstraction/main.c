10354515eff030ae045733c14de06e0e54e1f28e
Yifan Dai <daiy@rose-hulman.edu>
2021-02-22 23:26:50 -0500

    fixed some warning

--- driverlib_input_abstraction/main.c
+++ driverlib_input_abstraction/main.c
@@ -61,6 +61,16 @@ const eUSCI_SPI_SlaveConfig spiSlaveConfig =
         EUSCI_B_SPI_CLOCKPOLARITY_INACTIVITY_HIGH,
         EUSCI_B_SPI_3PIN
         };
+void transmitString(char *data, uint8_t len) {
+    //Just iterate over data and transmit each char
+    int index;
+    for (index = 0; index < len - 1; index++) {
+        UART_transmitData(EUSCI_A0_BASE, (uint8_t) data[index]);
+    }
+    UART_transmitData(EUSCI_A0_BASE, (uint8_t) '\n');   //Line Feed Escacpe
+    UART_transmitData(EUSCI_A0_BASE, (uint8_t) '\r');   //Carriage Return Escape
+}
+
 void init_UART(void){
         GPIO_setAsPeripheralModuleFunctionInputPin(UART_PORT, UART_TX | UART_RX, GPIO_PRIMARY_MODULE_FUNCTION);
         GPIO_setAsPeripheralModuleFunctionInputPin(botton_port1,  botton_A |  botton_B | botton_X |  botton_Y  |botton_trigger_left |botton_trigger_right| botton_Start |botton_Sel , GPIO_PRIMARY_MODULE_FUNCTION);
@@ -71,15 +81,6 @@ void init_UART(void){
         UART_enableModule(EUSCI_A0_BASE);
         transmitString(uartInit, length(uartInit));
 
-}
-void transmitString(char *data, uint8_t len) {
-    //Just iterate over data and transmit each char
-    int index;
-    for (index = 0; index < len - 1; index++) {
-        UART_transmitData(EUSCI_A0_BASE, (uint8_t) data[index]);
-    }
-    UART_transmitData(EUSCI_A0_BASE, (uint8_t) '\n');   //Line Feed Escacpe
-    UART_transmitData(EUSCI_A0_BASE, (uint8_t) '\r');   //Carriage Return Escape
 }
 
 
@@ -113,7 +114,57 @@ int main(void) {
 	    initLCD();
 	    testLCD();
 	    init_joy();
-	    init_button();
+	    MAP_CS_setReferenceOscillatorFrequency(CS_REFO_128KHZ);
+	           MAP_CS_initClockSignal(CS_MCLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
+	           MAP_CS_initClockSignal(CS_SMCLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_2);
+	           MAP_PCM_setPowerState(PCM_AM_LF_VCORE0);
+
+	           /* Configuring GPIO2.4 as peripheral output for PWM  and P6.7 for button
+	            * interrupt */
+
+	           MAP_GPIO_setAsInputPinWithPullUpResistor(botton_port1, botton_A);// A
+	           MAP_GPIO_setAsInputPinWithPullUpResistor(botton_port1, botton_B);// B
+	           MAP_GPIO_setAsInputPinWithPullUpResistor(botton_port1, botton_X);// X
+	           MAP_GPIO_setAsInputPinWithPullUpResistor(botton_port1, botton_Y);// Y
+	           MAP_GPIO_setAsInputPinWithPullUpResistor(botton_port1, botton_Start);// Start
+	           MAP_GPIO_setAsInputPinWithPullUpResistor(botton_port1, botton_Sel);// Select
+	           MAP_GPIO_setAsInputPinWithPullUpResistor(botton_port1, botton_trigger_left);// Trigger left
+	           MAP_GPIO_setAsInputPinWithPullUpResistor(botton_port1, botton_trigger_right);// Trigger right
+	           MAP_GPIO_setAsInputPinWithPullUpResistor(botton_port2, botton_P);// Pairing
+
+
+	           MAP_GPIO_clearInterruptFlag(botton_port1, botton_A);
+	           MAP_GPIO_clearInterruptFlag(botton_port1, botton_B);
+	           MAP_GPIO_clearInterruptFlag(botton_port1, botton_X);
+	           MAP_GPIO_clearInterruptFlag(botton_port1, botton_Y);
+	           MAP_GPIO_clearInterruptFlag(botton_port1, botton_Start);
+	           MAP_GPIO_clearInterruptFlag(botton_port1, botton_Sel);
+	           MAP_GPIO_clearInterruptFlag(botton_port1, botton_trigger_left);
+	           MAP_GPIO_clearInterruptFlag(botton_port1, botton_trigger_right);
+	           MAP_GPIO_clearInterruptFlag(botton_port2, botton_P);
+
+	           MAP_GPIO_enableInterrupt(botton_port1, botton_A);
+	           MAP_GPIO_enableInterrupt(botton_port1, botton_B);
+	           MAP_GPIO_enableInterrupt(botton_port1, botton_X);
+	           MAP_GPIO_enableInterrupt(botton_port1, botton_Y);
+	           MAP_GPIO_enableInterrupt(botton_port1, botton_Start);
+	           MAP_GPIO_enableInterrupt(botton_port1, botton_Sel);
+	           MAP_GPIO_enableInterrupt(botton_port1, botton_trigger_left);
+	           MAP_GPIO_enableInterrupt(botton_port1, botton_trigger_right);
+	           MAP_GPIO_enableInterrupt(botton_port2, botton_P);
+
+
+	           /* Configuring Timer_A to have a period of approximately 500ms and
+	            * an initial duty cycle of 10% of that (3200 ticks)  */
+
+	           //![Simple Timer_A Example]
+
+	           /* Enabling interrupts and starting the watchdog timer */
+	           MAP_Interrupt_enableInterrupt(INT_PORT1);
+
+
+	           MAP_Interrupt_enableSleepOnIsrExit();
+	           MAP_Interrupt_enableMaster();
 	    while((SysTick_CTRL_COUNTFLAG_Msk & SysTick-> CTRL) == 0){
 
 	    }
